b15424b12bfbd8a2a59f82a2f635771d460fb657
litchnom <40526763+litchnom@users.noreply.github.com>
2018-09-06 18:17:25 +0100

    first commit

---
+++ chapter1/ex1_6.cc
@@ -0,0 +1,13 @@
+/* The program fragment is illegal, because the << operator takes 
+ * two operands, but two operands are only present, for each such
+ * operator, in the first expression, on the first given line.
+ *
+ * The given fragment would be legal, if the semi-colon symbols
+ * were removed on the first and second line of the program 
+ * fragment.
+ */
+
+int main()
+{
+	return 0;
+}
