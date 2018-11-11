d89da2086b3d32c91554bf7492cbba497aadb195
litchnom <40526763+litchnom@users.noreply.github.com>
2018-11-11 11:56:54 +0000

    Exercises section 1.4.1 completed

---
+++ chapter1/ex10.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    int val = 10;
+    while(val >= 0) // while loop to print numbers from 10 to 0
+    {
+        std::cout << val << std::endl;
+        --val;
+    }
+}
---
+++ chapter1/ex9.cc
@@ -0,0 +1,15 @@
+// A program that uses a  while  to sum numbers from 50 to 100.
+#include <iostream>
+int main()
+{
+    int sum = 0, val = 50;
+    while(val <= 100)
+    {
+        sum = sum + val;
+        ++val;
+    }
+    std::cout << "The sum of values from 50 to 100 inclusive is "
+        << sum << std::endl;
+    
+    return 0;
+}
