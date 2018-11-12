0d591d6a98ee2f16e470183376f17927e7ce0298
litchnom <40526763+litchnom@users.noreply.github.com>
2018-11-12 21:15:03 +0000

    Section 1.4.2 partially completed

---
+++ chapter1/ex12.cc
@@ -0,0 +1,13 @@
+/* The for loop sums the numbers from  -100  to  100. Due to an inclusive conditition
+ * the final sum should be  0. */
+
+#include <iostream>
+int main()
+{
+    int sum = 0;
+    for(int i = -100; i <= 100; ++i)
+        sum += i;
+
+    std::cout << sum << std::endl;
+    return 0;
+}
---
+++ chapter1/ex14.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+int main()
+{
+    int sum = 0;
+    for(int i = -100; i <= 100; ++i)
+        sum += i;
+
+    int val = -100;
+    while(val <= 100)
+    {
+        sum += val;
+        ++val;
+    }
+
+    std::cout << sum << std::endl;
+    return 0;
+}
+
+/* After writing two equivalent loops, it seems that the advantage to the for loop
+ * is a conservation of space and easier readability due to the header taking care
+ * of a variable initialisation and incrementation and avoiding the need for curly
+ * braces due to the body being 1 line long. Something that could be viewed as either an
+ * advantage or disadvantage, depending on the scenario, is the difference in the 
+ * condition variable being defined outside the loop. It could be that the intention
+ * while is better suited towards conditions that require functions rather than simple
+ * iteration. */
